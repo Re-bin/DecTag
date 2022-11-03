f363b0da25a191d9eb665743f34e3a382ecbc183
rbin_1234 <1718954198@qq.com>
2022-11-03 17:41:15 +0800

    Update code

--- test_DecTag_LightGCN.py
+++ test_DecTag_LightGCN.py
@@ -18,6 +18,9 @@ torch.manual_seed(random_seed)
 torch.cuda.manual_seed(random_seed)
 np.random.seed(random_seed)
 random.seed(random_seed)
+torch.cuda.manual_seed_all(random_seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
 
 parser = argparse.ArgumentParser()
 
